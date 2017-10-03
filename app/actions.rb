b5d0c03842ca1bc90f5f3285e607498ecf1a6934
Kana Yanagawa <kana.yanagawa@outlook.com>
2017-10-03 06:07:18 +0000

    created login logout

--- app/actions.rb
+++ app/actions.rb
@@ -25,3 +25,25 @@ post '/signup' do
     end
 end
 
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user && user.password == password
+            session[:user_id] = user.id
+            "Success! User with id #{session[:user_id]} is logged in!"
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+end
